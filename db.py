41adc5652d3eda496407355cf34fc428fa170715
Mesailde Souza de Oliveira Matias <mesailde@ufscar.br>
2015-07-22 10:18:55 -0300

    Adiciona relacionamento com core.pessoa (ERP UFSCar)

--- db.py
+++ db.py
@@ -3,8 +3,8 @@
 
 from sqlalchemy import create_engine, \
      Column, ForeignKey, UniqueConstraint, \
-     BigInteger, Integer, String, TIMESTAMP, Boolean
-from sqlalchemy.dialects.postgresql import JSONB
+     BigInteger, Integer, String, DateTime, Date, Boolean
+from sqlalchemy.dialects.postgresql import JSONB, ENUM
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, relationship, backref
 import sys, datetime
@@ -20,23 +20,24 @@ class Item(Base):
                       {'schema': 'synclattes'})
 
     id = Column(BigInteger, primary_key=True)
-    id_cnpq = Column(String, nullable=False)
+    id_cnpq = Column(BigInteger, ForeignKey('synclattes.pessoa_lattes.id_cnpq'), nullable=False)
     seq_prod = Column(Integer, nullable=False)
     dspace_item_id = Column(BigInteger, nullable=True)
     dspace_cur_rev_id = Column(BigInteger, ForeignKey('synclattes.revision.id'), nullable=True)
     skip = Column(Boolean, nullable=False, default=False)
     frozen = Column(Boolean, nullable=False, default=False)
 
-    revisions = relationship('Revision',
-                             order_by='Revision.id',
-                             backref='item',
-                             foreign_keys='[Revision.item_id]')
+    pessoa_lattes = relationship('PessoaLattes', order_by=id,
+                                 backref='items', foreign_keys=[id_cnpq])
+
+    revisions = relationship('Revision', order_by='Revision.id',
+                             backref='item', foreign_keys='[Revision.item_id]')
 
     dspace_cur_rev = relationship('Revision', uselist=False,
                                   foreign_keys=[dspace_cur_rev_id])
 
     def __repr__(self):
-        return '<Item(id=%d, id_cnpq="%s", seq_prod=%d, dspace_item_id=%s, skip=%s, frozen=%s)>' % \
+        return '<Item(id=%s, id_cnpq=%s, seq_prod=%s, dspace_item_id=%s, skip=%s, frozen=%s)>' % \
                (self.id, self.id_cnpq, self.seq_prod, self.dspace_item_id, self.skip, self.frozen)
 
 
@@ -46,18 +47,57 @@ class Revision(Base):
 
     id = Column(BigInteger, primary_key=True)
     item_id = Column(BigInteger, ForeignKey('synclattes.item.id'), nullable=False)
-    retrieval_time = Column(TIMESTAMP, nullable=False, default=datetime.datetime.utcnow)
+    retrieval_time = Column(DateTime, nullable=False, default=datetime.datetime.utcnow)
     source = Column(String, nullable=False)
     meta = Column(JSONB, nullable=True)  # null se o item foi removido
     duplicate_of_id = Column(BigInteger, ForeignKey('synclattes.revision.id'), nullable=True)
 
-    duplicates = relationship('Revision',
-                              backref=backref('duplicate_of', remote_side=[id]),
+    duplicates = relationship('Revision', backref=backref('duplicate_of', remote_side=[id]),
                               foreign_keys=[duplicate_of_id])
 
     def __repr__(self):
-        return '<Revision(id=%d, item=%s, retrieval_time="%s", source="%s", meta=%s, duplicate_of_id=%s)>' % \
+        return '<Revision(id=%s, item=%s, retrieval_time="%s", source="%s", meta=%s, duplicate_of_id=%s)>' % \
                (self.id, self.item, self.retrieval_time, self.source, self.meta, self.duplicate_of_id)
+
+
+class PessoaLattes(Base):
+    __tablename__ = 'pessoa_lattes'
+    __table_args__ = {'schema': 'synclattes'}
+
+    id_cnpq = Column(BigInteger, primary_key=True, autoincrement=False)
+    pessoa_id = Column(BigInteger, ForeignKey('core.pessoa.id'), nullable=False)
+
+    def __repr__(self):
+        return '<PessoaLattes(id_cnpq=%s, pessoa_id=%s)>' % \
+               (self.id_cnpq, self.pessoa_id)
+
+
+class Pessoa(Base):
+    __tablename__ = 'pessoa'
+    __table_args__ = (UniqueConstraint('cpf'),
+                      {'schema': 'core'})
+
+    id = Column(BigInteger, primary_key=True)
+    version = Column(BigInteger, nullable=False)
+    cpf = Column(String(255), nullable=True)
+    data_nascimento = Column(Date, nullable=False)
+    email = Column(String(255), nullable=True)
+    idioma = Column(String(255), nullable=False)
+    nacionalidade_id = Column(BigInteger, nullable=False)
+    naturalidade_id = Column(BigInteger, nullable=True)
+    nome = Column(String(255), nullable=False)
+    passaporte = Column(String(255), nullable=True)
+    validade_visto = Column(Date, nullable=True)
+    sexo = Column(ENUM('M', 'F', name='sexo'), nullable=True)
+    alterado_manualmente = Column(Boolean, nullable=False, default=False)
+    date_created = Column(DateTime, nullable=True)
+    last_updated = Column(DateTime, nullable=True)
+
+    pessoa_lattes = relationship('PessoaLattes', uselist=False, backref='pessoa')
+
+    def __repr__(self):
+        return '<Pessoa(id=%s, cpf=%s, data_nascimento="%s", email="%s", nome="%s")>' % \
+               (self.id, self.cpf, self.data_nascimento, self.email, self.nome)
 
 
 if __name__ == '__main__':
