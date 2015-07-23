b8e7953e8ac91015191b44f5012e6fde17d06f45
Paulo Matias <matias@ufscar.br>
2015-07-22 23:37:31 -0300

    Add Python module for misc util functions

---
+++ util.py
@@ -0,0 +1,20 @@
+# -*- encoding: utf-8 -*-
+import re, unicodedata
+
+def onlyNumbers(s):
+    return re.sub(r'[^\d]', '', s)
+
+def norm(s, encoding='iso-8859-1'):
+    if not isinstance(s, unicode):
+        s = s.decode(encoding)
+    s = ''.join(c for c in unicodedata.normalize('NFD', s)
+               if unicodedata.category(c) != 'Mn')\
+        .encode('ascii','ignore')\
+        .strip()\
+        .lower()
+    return re.sub(r'\s+', ' ', s)
+
+def singleTag(tagList):
+    if len(tagList) != 1:
+        raise ValueError('len(%s) != 1' % repr(tagList))
+    return tagList[0]
