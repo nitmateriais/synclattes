edb6201bc6254ef92a8af717d46d00c89c811816
Mesailde Souza de Oliveira Matias <mesailde@ufscar.br>
2015-07-23 08:49:03 -0300

    Aumenta abstração para facilitar port para outras instituições

--- ufscar/pessoa.py
+++ ufscar/pessoa.py
@@ -1,8 +1,19 @@
 from sqlalchemy import or_, and_
 import db
 
-def getPessoa(cpfOrNumeroUFSCar):
-    return db.session.query(db.Pessoa).filter(or_(
-               db.Pessoa.cpf == cpfOrNumeroUFSCar,
-               db.Pessoa.id  == cpfOrNumeroUFSCar
-           )).one()
+class PessoaInstituicao(object):
+    def __init__(self, entidade):
+        self.entidade = entidade
+    @staticmethod
+    def fromIdentificador(cpfOrNumeroUFSCar):
+        return PessoaInstituicao(db.session.query(db.Pessoa).filter(or_(
+                   db.Pessoa.cpf == cpfOrNumeroUFSCar,
+                   db.Pessoa.id  == cpfOrNumeroUFSCar
+               )).one())
+    def getEntidade(self):
+        """ Entidade a ser inserida na chave estrangeira de PessoaLattes """
+        return self.entidade
+    def getCpf(self):
+        return self.entidade.cpf
+    def getPessoaLattes(self):
+        return self.entidade.pessoa_lattes
