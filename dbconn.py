0d1bb1335be1c574d3f37039bcacba7065eb8330
Mesailde Souza de Oliveira Matias <mesailde@ufscar.br>
2015-07-23 08:33:06 -0300

    Adiciona método para obter-ou-criar entidade

--- dbconn.py
+++ dbconn.py
@@ -1,9 +1,25 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.sql.expression import ClauseElement
 from sqlalchemy.orm import sessionmaker
 import conf.dbconf as dbconf
 
 engine = create_engine(dbconf.url)
 Base = declarative_base()
 Session = sessionmaker(bind=engine)
+
+# http://stackoverflow.com/a/2587041
+def get_or_create(session, model, defaults=None, **kwargs):
+    instance = session.query(model).filter_by(**kwargs).first()
+    if instance:
+        return instance, False
+    else:
+        params = dict((k, v) for k, v in kwargs.iteritems() if not isinstance(v, ClauseElement))
+        params.update(defaults or {})
+        instance = model(**params)
+        session.add(instance)
+        return instance, True
+
 session = Session()
+session.__class__.get_or_create = get_or_create
+
