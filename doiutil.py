ea349ee994205dad81d95881439a303d5345761d
Paulo Matias <matias@ufscar.br>
2015-07-23 22:52:01 -0300

    Adiciona novos metadados

---
+++ doiutil.py
@@ -0,0 +1,20 @@
+# -*- encoding: utf-8 -*-
+import logging
+
+SCHEME = 'doi:'
+RESOLVER = 'http://dx.doi.org'
+
+logger = logging.getLogger('doiutil')
+
+def toUrl(identifier):
+    # Based on DSpace's org.dspace.identifier.DOI#DOIToExternalForm
+    if identifier is None or identifier == '':
+        return None
+    if identifier.startswith(SCHEME):
+        return RESOLVER + '/' + identifier[len(SCHEME):]
+    if identifier.startswith('10.') and '/' in identifier:
+        return RESOLVER + '/' + identifier
+    if identifier.startswith(RESOLVER + '/10.'):
+        return identifier
+    logging.warning('Ignorando DOI inválido: %r', identifier)
+    return None
