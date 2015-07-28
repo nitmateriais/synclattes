ca39b3b2e0edeb25d2163afcf1853f4576a9dfa2
Paulo Matias <matias@ufscar.br>
2015-07-27 23:36:13 -0300

    Adiciona configurações mais detalhadas do simstring

--- conf/dedupconf.py
+++ conf/dedupconf.py
@@ -1,6 +1,9 @@
 # -*- encoding: utf-8 -*-
-
 import simstring
 
-titleMeasure = simstring.jaccard
-titleThreshold = 0.7
+# Configurações do simstring para busca de títulos similares
+
+titleNGram = 5   # Tamanho dos n-grams utilizados na geração do índice
+titleBE = False  # Inserir marcas especiais para começo e fim de strings nos n-grams?
+titleMeasure = simstring.jaccard  # Métrica de similaridade
+titleThreshold = 0.7              # Limiar de similaridade
