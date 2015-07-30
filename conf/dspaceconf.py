7b619b878fe3af71159013bece0245ad37d0dd34
Mesailde Souza de Oliveira Matias <mesailde@ufscar.br>
2015-07-30 08:07:04 -0300

    Adiciona configurações para sincronia

--- conf/dspaceconf.py
+++ conf/dspaceconf.py
@@ -1,3 +1,12 @@
 # -*- encoding: utf-8 -*-
 
+# Prefixo para gerar um identificador de autoridade a partir do ID do CV
 authorityPrefix = 'will be generated::lattes::'
+
+# Configurações para conexão ao SWORDv2
+serviceDocument = 'https://repositorion.ufscar.br/swordv2/servicedocument'
+username = 'admin'
+password = 'pass'
+
+# Coleção padrão de destino das entradas de metadados
+collectionIri = 'https://repositorion.ufscar.br/swordv2/collection/ufscar/2'
