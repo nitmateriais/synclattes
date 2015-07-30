478e9d3023d596f13aa3a9864c1c8f667c3e3719
Paulo Matias <matias@ufscar.br>
2015-07-30 17:09:09 -0300

    Adiciona configurações editIri e editMediaIri

--- conf/dspaceconf.py
+++ conf/dspaceconf.py
@@ -11,5 +11,13 @@ password = 'pass'
 # Coleção padrão de destino das entradas de metadados
 collectionIri = 'https://repositorion.ufscar.br/swordv2/collection/ufscar/2'
 
+# Endereço para obter o recibo de depósito de um item via SWORDv2
+editIri = 'https://repositorion.ufscar.br/swordv2/edit/%d'
+
+# Endereço para obter a declaração Atom de um item via SWORDv2
+editMediaIri = 'https://repositorion.ufscar.br/swordv2/edit-media/%d'
+
+# Endereço para obter o
+
 # Ponto de entrada da API RESTful (para operações não suportadas pelo SWORDv2)
 restUrl = 'https://repositorion.ufscar.br/rest'
